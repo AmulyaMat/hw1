5dea5b630ce92213bb66f9ccadbf527d15c0ae71
AmulyaMat <143415536+AmulyaMat@users.noreply.github.com>
2024-02-20 17:39:43 -0500

    Update train_q2.py

--- q1_q2_classification/train_q2.py
+++ q1_q2_classification/train_q2.py
@@ -7,6 +7,7 @@ import numpy as np
 import torchvision
 import torch.nn as nn
 import random
+from torchvision.models import ResNet18_Weights # added
 
 class ResNet(nn.Module):
     def __init__(self, num_classes) -> None:
